81d84539770eae5496984b797cc742e086c11486
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2021-12-10 23:44:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,46 +22,6 @@ df0.dropna(subset=["経度", "緯度"], inplace=True)
 
 df1 = df0[(df0["タイムスタンプ"] > dt_3dy) & (df0["確認"].isna())].drop(["タイムスタンプ", "確認"], axis=1)
 
-df1["場所"] = df1["市町村"].str.cat(df1["町名以降"])
-
-df1["備考"] = df1["備考"].fillna("")
-
-df1["eNB-LCID"] = df1["eNB"].str.cat(df1["LCID"], sep="-")
-
-df1["pid"] = df1["eNB"].astype(int).apply(lambda x: x >> 14)
-
-df1
-
-geo_df = gpd.GeoDataFrame(
-    df1, geometry=gpd.points_from_xy(df1["経度"], df1["緯度"]), crs=6668
-)
-
-ehime = gpd.read_file("N03-20210101_38_GML.zip!N03-20210101_38_GML").rename(
-    columns={"N03_001": "都道府県名", "N03_004": "市区町村名"}
-)
-ehime
-
-ehime.crs
-
-spj = gpd.sjoin(geo_df, ehime, how="left")
-spj
-
-spj["city"] = spj["市町村"] == spj["市区町村名"]
-spj["pref"] = spj["pid"] == 45
-
-spj
-
-df2 = spj.reindex(
-    columns=["場所", "緯度", "経度", "eNB-LCID", "pref", "city", "備考"]
-).copy()
-
-df2
-
-csv_path = pathlib.Path("map", "temp.csv")
-csv_path.parent.mkdir(parents=True, exist_ok=True)
-
-df2.to_csv(csv_path, encoding="utf_8_sig")
-
 kml = simplekml.Kml(name="temp")
 
 # 開局
@@ -89,21 +49,65 @@ kml.document.stylemaps.append(temp_stylemap)
 
 fol = kml.newfolder()
 
-for i, r in df2.iterrows():
+if len(df1) > 0:
 
-    pnt = fol.newpoint(name=r["場所"])
-    pnt.coords = [(r["経度"], r["緯度"])]
+    df1["場所"] = df1["市町村"].str.cat(df1["町名以降"])
 
-    pnt.stylemap = kml.document.stylemaps[0]
-    pnt.description = f'eNB-LCID: {r["eNB-LCID"]}'
+    df1["備考"] = df1["備考"].fillna("")
 
-    ex_data = simplekml.ExtendedData()
+    df1["eNB-LCID"] = df1["eNB"].str.cat(df1["LCID"], sep="-")
 
-    for n, v in r.items():
+    df1["pid"] = df1["eNB"].astype(int).apply(lambda x: x >> 14)
 
-        ex_data.newdata(name=str(n), value=str(v))
+    df1
 
-    pnt.extendeddata = ex_data
+    geo_df = gpd.GeoDataFrame(
+        df1, geometry=gpd.points_from_xy(df1["経度"], df1["緯度"]), crs=6668
+    )
+
+    ehime = gpd.read_file("N03-20210101_38_GML.zip!N03-20210101_38_GML").rename(
+        columns={"N03_001": "都道府県名", "N03_004": "市区町村名"}
+    )
+    ehime
+
+    ehime.crs
+
+    spj = gpd.sjoin(geo_df, ehime, how="left")
+    spj
+
+    spj["city"] = spj["市町村"] == spj["市区町村名"]
+    spj["pref"] = spj["pid"] == 45
+
+    spj
+
+    df2 = spj.reindex(
+        columns=["場所", "緯度", "経度", "eNB-LCID", "pref", "city", "備考"]
+    ).copy()
+
+    df2
+
+    csv_path = pathlib.Path("map", "temp.csv")
+    csv_path.parent.mkdir(parents=True, exist_ok=True)
+
+    df2.to_csv(csv_path, encoding="utf_8_sig")
+
+
+
+    for i, r in df2.iterrows():
+
+        pnt = fol.newpoint(name=r["場所"])
+        pnt.coords = [(r["経度"], r["緯度"])]
+
+        pnt.stylemap = kml.document.stylemaps[0]
+        pnt.description = f'eNB-LCID: {r["eNB-LCID"]}'
+
+        ex_data = simplekml.ExtendedData()
+
+        for n, v in r.items():
+
+            ex_data.newdata(name=str(n), value=str(v))
+
+        pnt.extendeddata = ex_data
 
 kmz_path = pathlib.Path("map", "temp.kmz")
 
